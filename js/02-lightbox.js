f4c96574be6009644eb650567bc5da87c3ccdc1e
Nikita <nicstyle22@gmail.com>
2022-05-31 18:52:40 +0300

    add 2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery"); // Обращение к родителю в HTML
+const imageMarkup = createImage(galleryItems); //Переменная для вызова функции
+
+galleryContainer.insertAdjacentHTML("beforeend", imageMarkup); //Добавление в родителя готовой разметки
+
+function createImage(image) {
+  //Рендер разметки
+  return image
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>`;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
