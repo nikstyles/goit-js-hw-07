91a2df8baeb57395ef7b5b4efb21754a15ba6b14
Nikita <nicstyle22@gmail.com>
2022-05-31 17:12:42 +0300

    add 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".js-gallery"); // Обращение к родителю в HTML
+const imageMarkup = createImage(galleryItems); //Переменная для вызова функции
+
+galleryContainer.insertAdjacentHTML("beforeend", imageMarkup); //Добавление в родителя готовой разметки
+
+galleryContainer.addEventListener("click", onGalleryContainerClick); //Делегирование прослушивания всей галереи
+
+function createImage(image) {
+  //Рендер разметки
+  return image
+    .map(({ preview, original, description }) => {
+      return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+            </div>`;
+    })
+    .join("");
+}
+
+let instance = null;
+
+function onGalleryContainerClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width = 100%>`
+  );
+
+  instance.show();
+}
